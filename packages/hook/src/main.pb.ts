81c0c8a282b9d0754525676fe50b5970c1cdd1c5
Satoshi <140612116+satohshi@users.noreply.github.com>
2025-12-05 15:14:35 +0900

    add types

--- packages/hook/src/main.pb.ts
+++ packages/hook/src/main.pb.ts
@@ -1,3 +1,5 @@
+type Endpoint = typeof import('./lib/endpoint')
+
 onBootstrap((e) => {
 	e.next()
 	require(`${__hooks}/handler.js`).default()
@@ -24,15 +26,15 @@ if ((require(`${__hooks}/config.json`) as Config).exposeEndpoint) {
 	if ((require(`${__hooks}/config.json`) as Config).secureEndpoint) {
 		// auth required
 		routerAdd('GET', `${(require(`${__hooks}/config.json`) as Config).endpointPath}`, (e) => {
-			require(`${__hooks}/lib/endpoint.js`).secureEndpointHandler(e)
+			;(require(`${__hooks}/lib/endpoint.js`) as Endpoint).secureEndpointHandler(e)
 		})
 		routerAdd('GET', '/logout', (e) => {
-			require(`${__hooks}/lib/endpoint.js`).logout(e)
+			;(require(`${__hooks}/lib/endpoint.js`) as Endpoint).logout(e)
 		})
 	} else {
 		// no auth required
 		routerAdd('GET', `${(require(`${__hooks}/config.json`) as Config).endpointPath}`, (e) => {
-			require(`${__hooks}/lib/endpoint.js`).renderSchema(e, false)
+			;(require(`${__hooks}/lib/endpoint.js`) as Endpoint).renderSchema(e, false)
 		})
 	}
 }
